862a1ee21bc73b0a59e3e3a3ad2460a33651afb9
nicolaslovento <31898579+nicolaslovento@users.noreply.github.com>
2018-06-07 12:14:17 -0300

    Add files via upload

---
+++ Practica_Pair_v1.2/Windows_32/Employee.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "ArrayList.h"
+#include "Employee.h"
+
+
+int employee_compare(void* pEmployeeA,void* pEmployeeB)
+{
+
+    int retorno;
+    Employee* e1=(Employee*)pEmployeeA;
+    Employee* e2=(Employee*)pEmployeeB;
+    retorno=-5;
+
+    if(e1!=NULL && e2!=NULL)
+    {
+        retorno=stricmp(e1->name,e2->name);
+    }
+    return retorno;
+}
+
+
+void employee_print(Employee* this)
+{
+    printf("\n********\nId: %d  \nName: %s  \nLastname: %s  \nOcupado: %d",this->id,this->name,this->lastName,this->isEmpty);
+}
+
+
+Employee* employee_new(void)
+{
+
+    Employee* returnAux = NULL;
+    //hacer malloc
+    return returnAux;
+
+}
+
+void employee_delete(Employee* this)
+{
+
+
+}
+
+int employee_setId(Employee* this, int id)
+{
+
+    return 0;
+
+}
+
+int employee_getId(Employee* this)
+{
+
+    return 0;
+
+}
+
+
---
+++ Practica_Pair_v1.2/Windows_32/main.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "ArrayList.h"
+#include "Employee.h"
+#include <string.h>
+/****************************************************
+    Menu:
+        1. Parse del archivo data.csv
+        2. Listar Empleados
+        3. Ordenar por nombre
+        4. Agregar un elemento
+        5. Elimina un elemento
+        6. Listar Empleados (Desde/Hasta)
+*****************************************************/
+
+
+int main()
+{
+    int aux;
+    int i;
+    int id,estado;
+    char nombre[20],apellido[20],boole[20];
+    ArrayList *lista;
+    //hacer un malloc para que apunte a una direccion de memoria.keep.
+    lista=al_newArrayList();//le pide memoria para usar el arraylist
+
+    FILE *pdata;
+
+    pdata=fopen("data.csv","r");
+
+    if(pdata==NULL)
+    {
+        printf("El archivo no pudo ser leido.\n");
+        exit(1);
+    }
+
+    do
+    {
+        int r;
+        r=fscanf(pdata,"%d,%s,%s,%s\n",&id,nombre,apellido,boole);
+        if(r==4)
+        {
+
+
+        printf("%d %s %s %s ",id,nombre,apellido,boole);
+        break;
+        }
+
+    }while(!feof(pdata));
+
+    fclose(pdata);
+
+
+
+    return 0;
+}
